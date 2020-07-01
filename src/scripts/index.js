f0825670d9147d2ac7e116c648871b3b25997949
LucasAdorno <lucasadorno2702@gmail.com>
2020-07-01 00:37:02 -0300

    style done

---
+++ src/scripts/index.js
@@ -0,0 +1,11 @@
+const
+
+document.querySelector('#addButton').onclick = () => {
+
+   const title = document.querySelector('#titleInput')
+   const content = document.querySelector('#contentInput')
+   const data = `${data.getDate()}/${data.getMonth()}/${data.getFullYear}`
+  
+
+
+}
